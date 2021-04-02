cc88cdd319ed22e7cebeae329b41ca097b8c9b9c
Venkat <Venkat@Venkat-PC>
2021-04-02 16:25:39 +0200

    Initialized the values for the board.

--- TicTacToe/src/main/java/com/bnp/kata/tictactoe/TicTacToe.java
+++ TicTacToe/src/main/java/com/bnp/kata/tictactoe/TicTacToe.java
@@ -2,14 +2,15 @@ package com.bnp.kata.tictactoe;
 
 public class TicTacToe {
 
-	public Character getPosition(int i, int j,char ch) {
-		
-		char[][] board=new char[3][3];
-		if(ch=='X')
-		return board[2][1]='X';
-      else{
-    	  return board[1][2]='O';
- }
-	}
+	public Character getPosition(int row, int column) {
 
+		char[][] board = new char[3][3];
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+				board[i][j] = 'X';
+			}
+		}
+		return board[row][column];
+
+	}
 }
--- TicTacToe/src/test/java/com/bnp/kata/tictactoe/TicTacToeTest.java
+++ TicTacToe/src/test/java/com/bnp/kata/tictactoe/TicTacToeTest.java
@@ -10,11 +10,17 @@ public class TicTacToeTest {
 	public void testPlayerPlaceXInBoard() {
 		TicTacToe ticTacToeTestObj = new TicTacToe();
 
-		assertThat(ticTacToeTestObj.getPosition(2, 1,'X'), CoreMatchers.is('X'));
+		assertThat(ticTacToeTestObj.getPosition(2, 1), CoreMatchers.is('X'));
 	}
 	@Test
-	public void testPlayerPlaceOInBoard() {
+	public void testPlayerPlaceXOnBoard() {
 		TicTacToe ticTacToeTestObj = new TicTacToe();
-		assertThat(ticTacToeTestObj.getPosition(1, 2,'O'), CoreMatchers.is('O'));
+		assertThat(ticTacToeTestObj.getPosition(1, 2), CoreMatchers.is('X'));
+	}
+	@Test
+	public void testPlayerShoulPlaceAnywhereOBoard()
+	{
+		TicTacToe ticTacToeTestObj = new TicTacToe();
+		ticTacToeTestObj.getPosition(0, 1);
 	}
 }
